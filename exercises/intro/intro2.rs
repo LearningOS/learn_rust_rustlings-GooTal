84d2769ae9f34fa761ac5e9d437f3a4fb717a0be
Qingtao Liu <qtliu@mail.ustc.edu.cn>
2022-10-19 13:38:37 +0000

    inro and variables finished

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -1,9 +1,6 @@
 // intro2.rs
 // Make the code print a greeting to the world.
 // Execute `rustlings hint intro2` for a hint.
-
-// I AM NOT DONE
-
 fn main() {
-    println!("Hello {}!");
+    println!("Hello {}!","Qingtao Liu");
 }
